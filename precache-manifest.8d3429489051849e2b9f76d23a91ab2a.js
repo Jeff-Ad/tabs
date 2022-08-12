18cf4fcd76e638ccd09483031fd90b067fcb998f
jeff-email <adakolejeffrey@gmail.com>
2022-08-12 18:38:31 +0100

    Updates

---
+++ precache-manifest.8d3429489051849e2b9f76d23a91ab2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd0ed063668c958c64d000936cfc5be",
+    "url": "/tour-site/index.html"
+  },
+  {
+    "revision": "3709a7da4b7eff4c97b7",
+    "url": "/tour-site/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/tour-site/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tour-site/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3709a7da4b7eff4c97b7",
+    "url": "/tour-site/static/js/main.4eb3586d.chunk.js"
+  },
+  {
+    "revision": "ca25126aa597c3ec8e2c",
+    "url": "/tour-site/static/js/runtime-main.eb8d1880.js"
+  }
+]);
